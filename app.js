88043f4cee2e5e79f1208311f183bc22502fbcc9
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2015-12-23 10:07:58 -0500

    small refactor to app

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
-
 var app = express();
 
 // all environments
@@ -26,5 +20,5 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Okize.com server listening on port ' + app.get('port'));
 });
